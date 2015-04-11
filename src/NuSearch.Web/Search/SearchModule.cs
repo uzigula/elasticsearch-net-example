f9f6c16a0e2b08c848a1a3786e0fdb475e4d99c8
gmarz <greg.marzouka@gmail.com>
2015-04-11 00:19:24 -0400

    Terms aggregation

--- src/NuSearch.Web/Search/SearchModule.cs
+++ src/NuSearch.Web/Search/SearchModule.cs
@@ -27,34 +27,50 @@ namespace NuSearch.Web.Search
 				var result = client.Search<Package>(s => s
 					.Size(25)
 					.Query(q => 
-						   q.Match(m => m
-							   .OnField(p => p.Id.Suffix("keyword"))
-							   .Boost(1000)
-							   .Query(form.Query)
-							) || 
-							q.FunctionScore(fs => fs
-								.MaxBoost(100)
-								.Functions(ff => ff
-									.FieldValueFactor(fvf => fvf
-										.Field(p => p.DownloadCount)
-										.Factor(0.0001)
-									)
+						q.Match(m => m
+							.OnField(p => p.Id.Suffix("keyword"))
+							.Boost(1000)
+							.Query(form.Query)
+						) || 
+						q.FunctionScore(fs => fs
+							.MaxBoost(100)
+							.Functions(ff => ff
+								.FieldValueFactor(fvf => fvf
+									.Field(p => p.DownloadCount)
+									.Factor(0.0001)
 								)
-								.Query(query => query
-									.MultiMatch(m => m
-										.OnFieldsWithBoost(fields => fields
-											.Add(p => p.Id.Suffix("keyword"), 1.5)
-											.Add(p => p.Id, 1.2)
-											.Add(p => p.Summary, 0.8)
-										)
-										.Operator(Operator.And)
-										.Query(form.Query)
+							)
+							.Query(query => query
+								.MultiMatch(m => m
+									.OnFieldsWithBoost(fields => fields
+										.Add(p => p.Id.Suffix("keyword"), 1.5)
+										.Add(p => p.Id, 1.2)
+										.Add(p => p.Summary, 0.8)
 									)
+									.Operator(Operator.And)
+									.Query(form.Query)
 								)
 							)
 						)
-					);
+					)
+					.Aggregations(a => a
+						.Nested("authors", n => n
+							.Path("authors")
+							.Aggregations(aa => aa
+								.Terms("author-names", ts => ts
+									.Field(p => p.Authors.First().Name)
+								)
+							)
+						)
+					)
+				);
 
+				var authors = result.Aggs.Nested("authors")
+					.Terms("author-names")
+					.Items
+					.ToDictionary(k => k.Key, v => v.DocCount);
+
+				model.Authors = authors;
 				model.Packages = result.Documents;
 				model.Total = result.Total;
 				model.Form = form;
